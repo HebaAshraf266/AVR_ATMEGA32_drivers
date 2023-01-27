ee838d7a8cd332a8456ddec72c99941ef18f5200
Heba Ashraf <115734048+HebaAshraf266@users.noreply.github.com>
2023-01-27 03:25:48 +0200

    Update main.c

--- LED & Buzzer/main.c
+++ LED & Buzzer/main.c
@@ -1,1 +1,23 @@
+#include "../SERV/STD_Types.h"
+#include "../SERV/BitMath.h"
+#include "../MCAL/DIO/DIO_int.h"
+#include "../HAL/LED_Buzzer/LEDsBuzzer_int.h"
 
+
+void main(void)
+{
+	LEDsBuzzer_vidInit(LED1);
+	LEDsBuzzer_u8TurnOff(LED1);
+	LEDsBuzzer_vidInit(LED2);
+	LEDsBuzzer_u8TurnOff(LED2);
+
+	while(1)
+	{
+		LEDsBuzzer_u8ToggleState(LED1);
+		LEDsBuzzer_u8TurnON(LED2);
+		_delay_ms(500);
+		LEDsBuzzer_u8TurnOff(LED2);
+		_delay_ms(500);
+    }
+
+}
